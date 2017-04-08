11ae956b0d21f9d1874e5456748781c679a9a597
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2017-04-08 12:57:06 -0400

    Added a http request

--- app/src/main/java/com/herokuapp/shopandgo/shopandgo/Login.java
+++ app/src/main/java/com/herokuapp/shopandgo/shopandgo/Login.java
@@ -2,6 +2,14 @@ package com.herokuapp.shopandgo.shopandgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +17,40 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login_button = (Button) findViewById(R.id.login);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText username = (EditText) findViewById(R.id.user_name);
+                AsyncHttpClient client = new AsyncHttpClient();
+                String user = username.getText().toString();
+                client.setBasicAuth(user,"password/token");
+                client.get("http://shopandgo.herokuapp.com/android/username/" + username.getText(), new AsyncHttpResponseHandler() {
+
+                    @Override
+                    public void onStart() {
+                        // called before request is started
+                    }
+
+                    @Override
+                    public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                        Log.e("GOT USERNAME", "GOT USERNAME");
+                    }
+
+                    @Override
+                    public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onRetry(int retryNo) {
+                        // called when request is retried
+                    }
+                });
+            }
+        });
+
     }
+
+
 }
